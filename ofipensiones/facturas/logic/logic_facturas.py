8dfc0f982717dfece9aecd542b56dcb6ab3297cd
acrodriguezs1 <ac.rodriguezs1@uniandes.edu.co>
2024-10-07 19:50:40 -0500

    baby baby baby ohhh

--- ofipensiones/facturas/logic/logic_facturas.py
+++ ofipensiones/facturas/logic/logic_facturas.py
@@ -1,5 +1,5 @@
 from ..models import Factura
 
-def get_factura(estudiante, fecha):
+def get_factura(var_pk):
     factura=Factura.objects.get(pk=var_pk)
     return factura
--- ofipensiones/facturas/views.py
+++ ofipensiones/facturas/views.py
@@ -3,6 +3,7 @@ from .logic import logic_facturas as lf
 from django.core import serializers
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from .models import Factura
 
 @csrf_exempt
 def factura_view(request, id):
@@ -12,4 +13,6 @@ def factura_view(request, id):
         return HttpResponse(factura, 'application/json')
         
             
-
+def mostrar_factura(request, id):
+    factura = get_object_or_404(Factura, id=factura_id)
+    return render(request, 'factura_detalle.html', {'factura': factura})
